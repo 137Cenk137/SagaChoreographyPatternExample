fa4cede454be62522f21a360f88ba002d36dce1f
Cenk Kıran <cenk33m@gmail.com>
2024-09-06 09:42:22 +0300

    a class that contains rabbitMQ setting(Quene name) was written

---
+++ Shared/RebbitMQSettings.cs
@@ -0,0 +1,7 @@
+
+namespace Shared;
+
+public static class RebbitMQSettings // metinsel  degerleri birkez tutacagımızdan dolayı static yaptık instance olmadan cagiracagimiz için
+{
+    public const string Stock_OrderCreatedEventQueue = "stock-order-created-event-queue";
+}
